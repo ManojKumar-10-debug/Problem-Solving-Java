06187cd728068a17b671337521666c2017172e98
ManojKumar <codingwithmanoj10@gmail.com>
2026-08-28 12:50:52 +0530

    Added comments

--- src/DataTypes/Add.java
+++ src/DataTypes/Add.java
@@ -2,6 +2,16 @@ package DataTypes;
 
 import java.util.Scanner;
 
+/* First take two variables
+             ||
+          Take input
+             ||
+        sum two number
+             ||
+          Store in c
+             ||
+           print c
+*/
 public class Add {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
